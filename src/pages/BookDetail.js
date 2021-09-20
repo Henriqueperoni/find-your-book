333d662b8c485ae9e13ae2755bf9e897b2ab180c
Henrique <henriqueperoni@hotmail.com>
2021-09-20 16:09:33 +0100

    Styled BookDetail page

--- src/pages/BookDetail.js
+++ src/pages/BookDetail.js
@@ -7,13 +7,28 @@ const BookDetail = ({ books }) => {
   const getBook = books.results.books.filter(
     (book) => book.rank === parseInt(id)
   );
-  console.log(getBook);
-  const { rank, title, author } = getBook[0];
-  console.log(rank);
+  const { rank, title, author, book_image, description, buy_links } =
+    getBook[0];
   return (
-    <div>
-      <h1>Book Detail Page</h1>
-      {title}
+    <div className="book-detail">
+      <div className="book-detail-img">
+        <img src={book_image} alt={`${title}'s cover'`} />
+      </div>
+      <div className="book-detail-desc">
+        <h1>{title}</h1>
+        <h3>{author}</h3>
+        <p>Rank Position: {rank}</p>
+        <p>{description}</p>
+        <div className="buy-btn-container">
+          {buy_links.map((link, index) => {
+            return (
+              <a className="buy-btn" key={index} href={link.url}>
+                {link.name}
+              </a>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
