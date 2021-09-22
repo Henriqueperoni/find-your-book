68055e13a729eee28a4661e875e13eb07220291d
Henrique <henriqueperoni@hotmail.com>
2021-09-22 09:22:46 +0100

    Add functionality to fetch data from especific month

--- src/App.js
+++ src/App.js
@@ -5,26 +5,49 @@ import Error from "./Components/Error";
 import BookDetail from "./pages/BookDetail";
 import Navbar from "./Components/Navbar";
 
-const NonfictionUrl = `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=${process.env.REACT_APP_BOOK_API_KEY}`;
-const howToUrl = `https://api.nytimes.com/svc/books/v3/lists/current/advice-how-to-and-miscellaneous.json?api-key=${process.env.REACT_APP_BOOK_API_KEY}`;
-
 function App() {
-  const [nonfictionBooks, setNonfictionBooks] = useState([]);
+  const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+      return `0${month}`;
+    } else {
+      return month;
+    }
+  };
+
+  const currentMonth = getCurrentMonth();
+
+  const [businessBooks, setBusinessBooks] = useState([]);
   const [howToBooks, setHowToBooks] = useState([]);
+  const [month, setMonth] = useState(parseInt(currentMonth));
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchNonfictionBooks = async () => {
-    const response = await fetch(NonfictionUrl);
+  const strMonth = (month) => {
+    if (month < 10) {
+      return `0${month}`;
+    } else {
+      return month;
+    }
+  };
+
+  const newMonth = strMonth(month);
+
+  const businessUrl = `https://api.nytimes.com/svc/books/v3/lists/2021-${newMonth}-01/business-books.json?api-key=${process.env.REACT_APP_BOOK_API_KEY}`;
+  const howToUrl = `https://api.nytimes.com/svc/books/v3/lists/2021-01-20/advice-how-to-and-miscellaneous.json?api-key=${process.env.REACT_APP_BOOK_API_KEY}`;
+
+  const fetchBooks = async () => {
+    const response = await fetch(businessUrl);
     const newBooks = await response.json();
-    setNonfictionBooks(newBooks);
+    setBusinessBooks(newBooks);
     const response1 = await fetch(howToUrl);
     const newBooks1 = await response1.json();
     setHowToBooks(newBooks1);
     setIsLoading(false);
   };
+
   useEffect(() => {
-    fetchNonfictionBooks();
-  }, []);
+    fetchBooks();
+  }, [month]);
 
   return (
     <BrowserRouter>
@@ -32,13 +55,15 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home
-            nonfictionBooks={nonfictionBooks}
+            businessBooks={businessBooks}
             howToBooks={howToBooks}
             isLoading={isLoading}
+            month={month}
+            setMonth={setMonth}
           />
         </Route>
         <Route path="/bookdetail/:id">
-          <BookDetail books={nonfictionBooks} isLoading={isLoading} />
+          <BookDetail books={businessBooks} isLoading={isLoading} />
         </Route>
         <Route path="/bookdetail1/:id">
           <BookDetail books={howToBooks} isLoading={isLoading} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,39 @@
 import React from "react";
 import Book from "../Components/Book";
 import { Link } from "react-router-dom";
-const Home = ({ nonfictionBooks, howToBooks, isLoading }) => {
+
+const Home = ({ businessBooks, howToBooks, isLoading, month, setMonth }) => {
   if (isLoading) {
     return <h1>Is Loading...</h1>;
   }
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   return (
     <div className="container">
+      <button onClick={() => setMonth(month - 1)}>Decrease</button>
+      {monthNames[month - 1]}
+      <button onClick={() => setMonth(month + 1)}>Increase</button>
       <h2>Nonfiction Best Sellers</h2>
       <section className="book-container">
-        {nonfictionBooks.results.books.map((book) => {
+        {businessBooks.results.books.map((book) => {
           return (
             <Link
               style={{ textDecoration: "none", color: "#0f0f0f" }}
-              books={nonfictionBooks}
+              books={businessBooks}
               key={book.rank}
               to={`/bookdetail/${book.rank}`}
             >
